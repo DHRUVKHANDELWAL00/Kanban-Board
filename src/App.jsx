b481020cfd0ab922b012bd24b36e2478ed149ab0
DHRUV KHANDELWAL <khandelwaldhruv2003@gmail.com>
2024-10-23 08:13:09 +0530

    chnages made

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
-import Cards from './components/Cards'
-import Card from './components/Cards/card'
 import StatusCard from './components/Status/StatusCard'
 import PriorityCard from './components/Priority/PriorityCard'
 import UserCard from './components/User/UserCard'
@@ -15,17 +11,12 @@ function App() {
   return (
     <>
             <Navbar count={count} setCount={setCount} sort={sort} setSort={setSort}/>
-            {/* <Cards/>
-            <Card/> */}
-            {/* {count===0 && <Card sort={sort}/>} */}
+            
             {console.log("count is ",count)}
             {count==1 &&  <UserCard sort={sort}/>}
             {count==2 && <PriorityCard sort={sort}/>}
             {count==3 && <StatusCard sort={sort}/>}
-            {/* <UserCard sort={sort}/>
-            <PriorityCard sort={sort}/>
-            <StatusCard sort={sort}/>
-             */}
+            
             
             
             
